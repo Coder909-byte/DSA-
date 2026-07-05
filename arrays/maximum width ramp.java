31d36fb8343840f8438296feae04887a3635d198
Jivesh Kumar Nazar <jivesh.nazar@gmail.com>
2026-07-05 17:14:40 +0530

    maximum width ramp

---
+++ arrays/maximum width ramp.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        
+        // Step 1: Build the stack
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[i] < nums[stack.peek()]) {
+                stack.push(i);
+            }
+        }
+        
+        int maxWidth = 0;
+        
+        // Step 2: Traverse backwards
+        for (int j = n - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums[j] >= nums[stack.peek()]) {
+                maxWidth = Math.max(maxWidth, j - stack.pop());
+            }
+        }
+        
+        return maxWidth;
+    }
+}
